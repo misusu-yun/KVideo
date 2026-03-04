b2cc05a13ac8119da870e8981fa01d881772b3e2
misusu-yun <126746718+misusu-yun@users.noreply.github.com>
2026-03-04 14:05:44 +0800

    Update route.ts

--- app/api/iptv/route.ts
+++ app/api/iptv/route.ts
@@ -5,7 +5,6 @@
 export const runtime = 'edge';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const runtime = 'nodejs';
 
 export async function GET(request: NextRequest) {
   const url = request.nextUrl.searchParams.get('url');
